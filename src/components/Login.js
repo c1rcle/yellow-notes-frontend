50f7d70ab965f2d74008ae46d9d4c5145c8d69c3
jaqb8 <qba.anisz@gmail.com>
2020-03-10 22:07:52 +0100

    add fontawesome icons to login component

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,28 +1,48 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { Form, Button, Row, Col } from 'react-bootstrap';
 
 const Login = () => {
   return (
-    <Row>
-      <Col md={{ span: 6, offset: 3 }} className='my-5'>
-        <Form>
-          <Form.Group controlId='emailField'>
-            <Form.Control type='email' placeholder='Email Address' />
-          </Form.Group>
-          <Form.Group controlId='passwordField'>
-            <Form.Control type='password' placeholder='Password' />
-          </Form.Group>
-          <Button className='btn-block' variant='outline-primary'>
-            Login
-          </Button>
-          <hr />
-          <p className='lead text-center'>
-            Or <Button variant='outline-success'>Register</Button> to create new
-            account!
-          </p>
-        </Form>
-      </Col>
-    </Row>
+    <Fragment>
+      <Row>
+        <Col md={{ span: 8, offset: 2 }} className='my-4'>
+          <h1 className='display-4 text-center'>Welcome to Yellow Notes!</h1>
+        </Col>
+      </Row>
+      <Row>
+        <Col md={{ span: 6, offset: 3 }} className='my-2'>
+          <Form>
+            <Form.Group as={Row} controlId='emailField'>
+              <Form.Label column xs='2' sm='1' className='pl-4'>
+                <i className='fas fa-at'></i>
+              </Form.Label>
+              <Col xs='10' sm='11'>
+                <Form.Control type='email' placeholder='Email Address' />
+              </Col>
+            </Form.Group>
+            <Form.Group as={Row} controlId='passwordField'>
+              <Form.Label column xs='2' sm='1' className='pl-4'>
+                <i className='fas fa-hashtag'></i>
+              </Form.Label>
+              <Col xs='10' sm='11'>
+                <Form.Control type='password' placeholder='Password' />
+              </Col>
+            </Form.Group>
+            <Button className='btn-block' variant='outline-primary'>
+              <i className='fas fa-home mr-1'></i> Login
+            </Button>
+            <hr />
+            <p className='lead text-center'>
+              Or{' '}
+              <Button variant='outline-success'>
+                <i className='fas fa-user-plus mr-2'></i>Register
+              </Button>{' '}
+              to create new account!
+            </p>
+          </Form>
+        </Col>
+      </Row>
+    </Fragment>
   );
 };
 
