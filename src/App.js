a576054773581990562b8f029fd6b878c7236f80
Jan Miszczyk <jachuuu_15@hotmail.com>
2020-03-11 00:27:26 +0100

    Implemented design

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import Registration from './components/Registration';
+import { Container } from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
+      <Container>
+        <Registration />
+      </Container>
     </div>
   );
 }
