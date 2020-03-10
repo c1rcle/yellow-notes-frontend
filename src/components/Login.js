b5613fd853ea488aad6cb902141e0bc2a2384c9d
jaqb8 <qba.anisz@gmail.com>
2020-03-10 21:20:44 +0100

    switched to styling with react-bootstrap components

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,32 +1,29 @@
-import React, { Fragment } from 'react';
-import { Form, Button } from 'react-bootstrap';
+import React from 'react';
+import { Form, Button, Row, Col } from 'react-bootstrap';
 
 const Login = () => {
   return (
-    <Fragment>
-      <Form style={formStyle}>
-        <Form.Group controlId='emailField'>
-          <Form.Control type='email' placeholder='Emaill Address' />
-        </Form.Group>
-        <Form.Group controlId='passwordField'>
-          <Form.Control type='password' placeholder='Password' />
-        </Form.Group>
-        <Button className='btn-block' variant='outline-primary'>
-          Login
-        </Button>
-        <hr />
-        <p className='lead text-center'>
-          Or <Button variant='outline-success'>Register</Button> to create new
-          account!
-        </p>
-      </Form>
-    </Fragment>
+    <Row>
+      <Col md={{ span: 6, offset: 3 }} className='my-5'>
+        <Form>
+          <Form.Group controlId='emailField'>
+            <Form.Control type='email' placeholder='Email Address' />
+          </Form.Group>
+          <Form.Group controlId='passwordField'>
+            <Form.Control type='password' placeholder='Password' />
+          </Form.Group>
+          <Button className='btn-block' variant='outline-primary'>
+            Login
+          </Button>
+          <hr />
+          <p className='lead text-center'>
+            Or <Button variant='outline-success'>Register</Button> to create new
+            account!
+          </p>
+        </Form>
+      </Col>
+    </Row>
   );
-};
-
-const formStyle = {
-  maxWidth: '500px',
-  margin: '2rem auto'
 };
 
 export default Login;
