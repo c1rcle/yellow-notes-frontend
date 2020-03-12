4977e44b3ab9256de084866340a4377c53effead
Filip <filip.k.wanat@gmail.com>
2020-03-12 10:14:55 +0100

    Implement custom Tooltip using react-boostrap

---
+++ src/components/common/TooltipCustom.js
@@ -0,0 +1,26 @@
+import React, { useRef } from 'react';
+import { Overlay, Tooltip } from 'react-bootstrap';
+
+const TooltipCustom = props => {
+  const ref = useRef();
+
+  let child = Array.isArray(props.children) ? props.children[0] : props.children;
+  if (child === undefined) {
+    child = <div ref={ref} />;
+  } else if (child.ref === null) {
+    throw new Error('First child has to have defined ref');
+  }
+  console.log('props', props);
+  const { text, isOpen, placement } = props;
+  const target = child.ref.current;
+  return (
+    <>
+      {props.children || child}
+      <Overlay target={target} show={isOpen} placement={placement}>
+        {props => <Tooltip {...props}>{text}</Tooltip>}
+      </Overlay>
+    </>
+  );
+};
+
+export default TooltipCustom;
