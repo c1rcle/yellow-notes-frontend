693e42f89cadaf279172b9ecc49a7df5a17840d8
Wojtek <vopox07@gmail.com>
2020-03-14 13:47:52 +0100

    Move /notes to /pages/notes

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
 import Registration from './components/pages/Registration';
 import Login from './components/pages/Login';
-import NoteContainer from './components/notes/NoteContainer'
+import NoteContainer from './components/pages/notes/NoteContainer';
 
 function App() {
   return (
@@ -12,7 +12,7 @@ function App() {
         <Switch>
           <Route exact path='/' component={Login} />
           <Route exact path='/registration' component={Registration} />
-          <Route exact path='/notes' component={NoteContainer}/>
+          <Route exact path='/notes' component={NoteContainer} />
         </Switch>
       </Container>
     </Router>
