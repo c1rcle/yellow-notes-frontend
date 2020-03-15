9b0ca9acaceb518e12e71ff258778e7d05a05273
MateNaw <49524385+MateNaw@users.noreply.github.com>
2020-03-15 23:27:31 +0100

    added navbar component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
-import React from 'react';
-import { BrowserRouter as Router, Switch } from 'react-router-dom';
-import { Container } from 'react-bootstrap';
-import Registration from './components/pages/Registration';
-import Login from './components/pages/Login';
-import Notes from './components/pages/Notes';
-import FadingRoute from './components/common/FadingRoute';
+import React from "react";
+import { BrowserRouter as Router, Switch } from "react-router-dom";
+import { Container } from "react-bootstrap";
+import Registration from "./components/pages/Registration";
+import Login from "./components/pages/Login";
+import Notes from "./components/pages/Notes";
+import FadingRoute from "./components/common/FadingRoute";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <Router>
+      <Navbar />
+      <main style={{ marginTop: "78px" }}></main>
       <Container>
         <Switch>
-          <FadingRoute exact key='0' path='/' component={Login} />
-          <FadingRoute exact key='1' path='/registration' component={Registration} />
-          <FadingRoute exact key='2' path='/notes' component={Notes} />
+          <FadingRoute exact key="0" path="/" component={Login} />
+          <FadingRoute
+            exact
+            key="1"
+            path="/registration"
+            component={Registration}
+          />
+          <FadingRoute exact key="2" path="/notes" component={Notes} />
         </Switch>
       </Container>
     </Router>
