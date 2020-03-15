2ced96b31f97f7ee6b307cdff09411744b910120
Jan Miszczyk <jachuuu_15@hotmail.com>
2020-03-15 17:57:12 +0100

    Fixed notes styling

--- src/components/common/FadingRoute.js
+++ src/components/common/FadingRoute.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { Fade } from 'react-bootstrap';
 import { Route } from 'react-router-dom';
 
--- src/components/pages/Notes/Note.js
+++ src/components/pages/Notes/Note.js
@@ -1,15 +1,17 @@
 import React from 'react';
-import { Card } from 'react-bootstrap';
+import { Card, Form } from 'react-bootstrap';
 import './notes.css';
 
 const Note = props => {
   const { content } = props;
   return (
-    <div className='note-item'>
-      <Card.Body disabled='true' as='textarea' rows='5'>
-        {content}
+    <Card className='shadow-sm'>
+      <Card.Body className='p-0'>
+        <Form.Control disabled as='textarea' className='note-item' rows={5}>
+          {content}
+        </Form.Control>
       </Card.Body>
-    </div>
+    </Card>
   );
 };
 
