803eee4d82a0ed667c9dc5e6efa8207af7a8048a
Filip <filip.k.wanat@gmail.com>
2020-03-17 16:27:55 +0100

    Add temporary add note panel

--- src/components/pages/Notes/index.js
+++ src/components/pages/Notes/index.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Note from './Note';
-import { Row, Col } from 'react-bootstrap';
+import { Row, Col, ListGroupItem, ListGroup, Form, Button } from 'react-bootstrap';
 import useUser from '../../../contexts/UserContext';
 import { Redirect } from 'react-router-dom';
 
@@ -9,8 +9,9 @@ const Notes = () => {
   console.log(user);
   return (
     <>
+      <AddNote />
       <Row>
-        {user.email !== undefined || <Redirect to='/' />}
+        {user.isUserLoggedIn || <Redirect to='/' />}
         {!user.notes ||
           user.notes.map(note => (
             <Col lg={4} className='mt-3' key={note.id}>
@@ -23,3 +24,34 @@ const Notes = () => {
 };
 
 export default Notes;
+
+function AddNote() {
+  const [, dispatch] = useUser();
+  const [note, setNote] = useState('');
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    if (note === '') return;
+    dispatch({ type: 'ADD_NOTE', payload: { content: note } });
+    console.log(note);
+    setNote('');
+  };
+
+  return (
+    <ListGroup className='mt-3 mb-3'>
+      <ListGroupItem>
+        <Form className='d-flex' onSubmit={handleSubmit}>
+          <Form.Control
+            type='text'
+            placeholder='New note'
+            value={note}
+            onChange={e => setNote(e.target.value)}
+          />
+          <Button className='btn-lg ml-3 p-0' type='submit'>
+            <i className='fas fa-plus mr-2 ml-2' />
+          </Button>
+        </Form>
+      </ListGroupItem>
+    </ListGroup>
+  );
+}
