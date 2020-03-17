d92a85dbd459dff56c803546d68f7b7d45753fff
Jan Miszczyk <jachuuu_15@hotmail.com>
2020-03-17 18:54:30 +0100

    Updates

---
+++ precache-manifest.a5e00c28c38b2710f3563599adf2068b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b3da2db8c4a4878fdbee52042594943",
+    "url": "/yellow-notes-frontend/index.html"
+  },
+  {
+    "revision": "9f3b36ad192db848441d",
+    "url": "/yellow-notes-frontend/static/css/2.a2565aac.chunk.css"
+  },
+  {
+    "revision": "8818ccec6e438dc4d0ca",
+    "url": "/yellow-notes-frontend/static/css/main.966f56b0.chunk.css"
+  },
+  {
+    "revision": "9f3b36ad192db848441d",
+    "url": "/yellow-notes-frontend/static/js/2.8673851f.chunk.js"
+  },
+  {
+    "revision": "df8be702f0cc9d299c0ff42ca03d3e40",
+    "url": "/yellow-notes-frontend/static/js/2.8673851f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8818ccec6e438dc4d0ca",
+    "url": "/yellow-notes-frontend/static/js/main.5ade8ebe.chunk.js"
+  },
+  {
+    "revision": "1de72f40d487212f2fd7",
+    "url": "/yellow-notes-frontend/static/js/runtime-main.ca0347dc.js"
+  }
+]);
