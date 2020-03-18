9660c75875a06ef04c7ac6c23f7bab329595382f
Filip <filip.k.wanat@gmail.com>
2020-03-18 18:54:06 +0100

    add addNoteAction

---
+++ src/contexts/UserContext/actions/addNoteAction.js
@@ -0,0 +1,26 @@
+// TODO API - uncomment
+//import yellowNotesApi from '../../apis/yellowNotesApi';
+
+const addNoteAction = async action => {
+  if (!action.payload || !action.payload.content || Object.keys(action.payload).length !== 1)
+    throw new Error('Registration request has invalid parameters!');
+
+  let response;
+  try {
+    // TODO API - remove and uncomment
+    response = {
+      status: 200,
+      data: { content: action.payload.content }
+    };
+    //response = await yellowNotesApi.post('user/note', { ...action.payload });
+  } catch (e) {
+    throw new Error('Registration request has did not succeed! ', response);
+  }
+
+  if (response.status !== 200)
+    throw new Error('Registration request has did not succeed! ', response);
+
+  return { ...action, payload: response.data };
+};
+
+export default addNoteAction;
