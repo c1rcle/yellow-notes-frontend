3dcdcb4b6259c8a3f4ca8af62655a7977b6ccdd6
Filip <filip.k.wanat@gmail.com>
2020-03-18 18:34:39 +0100

    Add register action in userContext

---
+++ src/contexts/UserContext/actions/registerAction.js
@@ -0,0 +1,30 @@
+//import yellowNotesApi from '../../apis/yellowNotesApi';
+
+export default registerAction = async action => {
+  if (
+    !action.payload ||
+    !action.payload.password ||
+    !action.payload.email ||
+    Object.keys(action.payload).length !== 2
+  )
+    throw new Error('Registration request has invalid parameters!');
+
+  let response;
+  try {
+    response = {
+      status: 200,
+      data: { email: action.payload.email }
+    };
+    //response = await yellowNotesApi.post('user/register', { ...action.payload });
+  } catch (e) {
+    throw new Error('Registration request has did not succeed! ', response);
+  }
+
+  if (response.status !== 200)
+    throw new Error('Registration request has did not succeed! ', response);
+
+  action.payload = response.data;
+  //action.payload.notes || (action.payload.notes = []);
+  console.log(action.payload);
+  return action;
+};
