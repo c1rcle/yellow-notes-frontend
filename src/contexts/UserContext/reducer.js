1e1f6d08dd6359a050f461ca0dc129085380d88b
Filip <filip.k.wanat@gmail.com>
2020-03-18 18:48:06 +0100

    secure reducer from undefined notes in userContext

--- src/contexts/UserContext/reducer.js
+++ src/contexts/UserContext/reducer.js
@@ -5,8 +5,10 @@ export default (state = { isUserLoggedIn: false }, { type, payload }) => {
     case 'LOGIN':
       return { ...payload, isUserLoggedIn: true };
     case 'ADD_NOTE':
-      const newId = state.notes.length === 0 ? 0 : state.notes[state.notes.length - 1].id + 1;
-      return { ...state, notes: [...state.notes, { ...payload, id: newId }] };
+      let newId = 0;
+      if (!!state.notes)
+        newId = state.notes.length === 0 ? 0 : state.notes[state.notes.length - 1].id + 1;
+      return { ...state, notes: [...(state.notes || []), { ...payload, id: newId }] };
     case 'LOGOUT':
       return { isUserLoggedIn: false };
     default:
