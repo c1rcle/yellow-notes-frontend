4edc724ead3a2d23ef66f32e6eb03d92f2192f60
Filip Wanat <filip.k.wanat@gmail.com>
2020-03-26 17:40:26 +0100

    Add isLoading to userContext #24

--- src/contexts/UserContext/dispatchAsync.js
+++ src/contexts/UserContext/dispatchAsync.js
@@ -7,16 +7,19 @@ import checkTokenAction from './actions/checkTokenAction';
 const dispatchAsync = dispatch => action => {
   switch (action.type) {
     case 'CHECK_TOKEN':
+      dispatch({ type: 'LOADING_START' });
       checkTokenAction(action)
         .then(a => dispatch(a))
         .catch(err => console.log(err));
       break;
     case 'REGISTER':
+      dispatch({ type: 'LOADING_START' });
       registerAction(action)
         .then(a => dispatch(a))
         .catch(err => console.log(err));
       break;
     case 'LOGIN':
+      dispatch({ type: 'LOADING_START' });
       loginAction(action)
         .then(a => dispatch(a))
         .catch(err => console.log(err));
@@ -26,6 +29,7 @@ const dispatchAsync = dispatch => action => {
       dispatch(action);
       break;
     case 'ADD_NOTE':
+      dispatch({ type: 'LOADING_START' });
       addNoteAction(action)
         .then(a => dispatch(a))
         .catch(err => console.log(err));
