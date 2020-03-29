888fadf23a23f08d5c69eadfcebf02814691a0ad
Filip Wanat <filip.k.wanat@gmail.com>
2020-03-29 15:23:12 +0200

    Add checkbox tooltip #36

--- src/components/pages/Registration/Checkbox.js
+++ src/components/pages/Registration/Checkbox.js
@@ -1,17 +1,20 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { FormGroup, Form } from 'react-bootstrap';
+import TooltipCustom from '../../common/TooltipCustom';
 
-const Checkbox = ({ onClick }) => {
+const Checkbox = ({ onClick, tooltipText, tooltipEnabled }) => {
   return (
-    <FormGroup className='row justify-content-center my-4'>
-      <Form.Check
-        custom
-        id='1'
-        type='checkbox'
-        label='I agree to the terms and conditions'
-        onClick={onClick}
-      />
-    </FormGroup>
+    <TooltipCustom text={tooltipText} show={!!tooltipText && tooltipEnabled} placement='right'>
+      <FormGroup className='row justify-content-center my-4' ref={useRef()}>
+        <Form.Check
+          custom
+          id='1'
+          type='checkbox'
+          label='I agree to the terms and conditions'
+          onClick={onClick}
+        />
+      </FormGroup>
+    </TooltipCustom>
   );
 };
 
