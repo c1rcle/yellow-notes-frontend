ad1a05c50fd8b87a7e7fdfd2ec76a90ddee3db06
Filip Wanat <filip.k.wanat@gmail.com>
2020-04-03 22:40:56 +0200

    Fix minor codefactor hiccups #31

--- src/components/pages/Notes/Note.js
+++ src/components/pages/Notes/Note.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Card, Form } from 'react-bootstrap';
-import '../../../styles/notes.css'
+import '../../../styles/notes.css';
 import useNotes from '../../../contexts/NotesContext';
 
 const Note = ({ note }) => {
