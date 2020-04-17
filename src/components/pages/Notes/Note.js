6867e48f1ab3ed92d977627524c2e2f5e5c41290
Wojtek <vopox07@gmail.com>
2020-04-17 20:09:31 +0200

    Fix code factor issue

--- src/components/pages/Notes/Note.js
+++ src/components/pages/Notes/Note.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Card, Button } from 'react-bootstrap';
 import SimpleBar from 'simplebar-react';
 import useNotes from '../../../contexts/NotesContext';
