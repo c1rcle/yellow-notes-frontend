08b6b8ce19b4ef3f4e69eb1472369880d8a078a6
Filip Wanat <filip.k.wanat@gmail.com>
2020-04-29 18:43:48 +0200

    Implement note dialog title autofocus #102

--- src/components/pages/Notes/NoteDialog/NoteDialogForm.js
+++ src/components/pages/Notes/NoteDialog/NoteDialogForm.js
@@ -1,15 +1,22 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { Modal, Form } from 'react-bootstrap';
 import Todo from '../Todo';
 import { getVariant, getFormColor } from '../../../../utility/colorUtility';
 
 const NoteDialogForm = props => {
-  const { isNoteNew, children, onSubmit, formData, setFormData } = props;
+  const { isNoteNew, children, onSubmit, formData, setFormData, dialogVisible } = props;
   const { title, content, color, isBlocked } = formData;
+  const inputElement = useRef(null);
 
   const onChange = e => {
     e.target && setFormData({ ...formData, [e.target.name]: e.target.value });
   };
+
+  useEffect(() => {
+    if (dialogVisible && inputElement.current) {
+      inputElement.current.focus();
+    }
+  }, [dialogVisible]);
 
   return (
     <Form onSubmit={onSubmit}>
@@ -21,13 +28,14 @@ const NoteDialogForm = props => {
         <Modal.Title style={{ width: '100%' }}>
           <Form.Control
             readOnly={isBlocked && !isNoteNew}
+            ref={inputElement}
             name='title'
             value={title}
             onChange={e => onChange(e)}
             type='text'
             placeholder='Note Title'
             tabIndex='1'
-            className={`text-${getVariant(color)} placeholder-${getVariant(color)}`}
+            className={`text-${getVariant(color)} placeholder-${getVariant(color)} autofocus`}
             style={{ backgroundColor: getFormColor(color), borderWidth: '0' }}
           />
         </Modal.Title>
