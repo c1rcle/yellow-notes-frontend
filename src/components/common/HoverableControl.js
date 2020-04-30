068f8f6a7dc6e8f8769a19edc1c32bcb348446a7
Jan Miszczyk <jachuuu_15@hotmail.com>
2020-04-30 03:04:28 +0200

    Moved hover state to a separate component

---
+++ src/components/common/HoverableControl.js
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+import { Form } from 'react-bootstrap';
+import { getFormColor } from '../../utility/colorUtility';
+
+const HoverableControl = props => {
+  const [state, setState] = useState({ hover: false, focus: false });
+
+  return (
+    <Form.Control
+      onMouseOver={() => setState({ ...state, hover: true })}
+      onMouseOut={() => setState({ ...state, hover: false })}
+      onFocus={() => setState({ ...state, focus: true })}
+      onBlur={() => setState({ ...state, focus: false })}
+      style={{
+        backgroundColor: state.hover || state.focus ? getFormColor(props.color) : props.color
+      }}
+      {...props}
+    />
+  );
+};
+
+export default HoverableControl;
--- src/components/pages/Notes/NoteDialog/NoteDialogForm.js
+++ src/components/pages/Notes/NoteDialog/NoteDialogForm.js
@@ -1,13 +1,13 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef } from 'react';
 import { Modal, Form } from 'react-bootstrap';
+import HoverableControl from '../../../common/HoverableControl';
 import Todo from '../Todo';
 import { getVariant, getFormColor } from '../../../../utility/colorUtility';
 
 const NoteDialogForm = props => {
-  const [titleState, setTitleState] = useState({ hover: false, focus: false });
   const { isNoteNew, children, onSubmit, formData, setFormData, dialogVisible } = props;
   const { title, content, color, isBlocked } = formData;
-  
+
   const inputElement = useRef(null);
 
   const onChange = e => {
@@ -28,25 +28,19 @@ const NoteDialogForm = props => {
           borderBottom: 'none'
         }}>
         <Modal.Title style={{ width: '100%' }}>
-          <Form.Control
+          <HoverableControl
+            type='text'
             readOnly={isBlocked && !isNoteNew}
             ref={inputElement}
             name='title'
             value={title}
-            onChange={e => onChange(e)}
-            type='text'
             placeholder='Note Title'
+            onChange={e => onChange(e)}
             tabIndex='1'
+            color={color}
             className={`note-title border-0 text-${getVariant(color)} placeholder-${getVariant(
               color
             )}`}
-            style={{
-              backgroundColor: titleState.hover || titleState.focus ? getFormColor(color) : color
-            }}
-            onMouseOver={() => setTitleState({ ...titleState, hover: true })}
-            onMouseOut={() => setTitleState({ ...titleState, hover: false })}
-            onFocus={() => setTitleState({ ...titleState, focus: true })}
-            onBlur={() => setTitleState({ ...titleState, focus: false })}
           />
         </Modal.Title>
       </Modal.Header>
@@ -54,13 +48,13 @@ const NoteDialogForm = props => {
       <Modal.Body style={{ backgroundColor: color }}>
         {formData.variant === 0 ? (
           <Form.Control
+            as='textarea'
             readOnly={isBlocked && !isNoteNew}
             name='content'
             value={content}
-            onChange={e => onChange(e)}
-            as='textarea'
-            rows='3'
             placeholder='Note Content'
+            onChange={e => onChange(e)}
+            rows='3'
             tabIndex='2'
             className={`border-0 text-${getVariant(color)} placeholder-${getVariant(color)}`}
             style={{ backgroundColor: getFormColor(color) }}
