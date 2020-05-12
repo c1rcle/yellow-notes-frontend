621d092edc8b3103f4047bb1b56241d2a245896f
Jan Miszczyk <jachuuu_15@hotmail.com>
2020-05-12 21:22:34 +0200

    Changed tooltips base to react-tiny-popover

---
+++ src/components/common/CustomTooltip.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Popover, { ArrowContainer } from 'react-tiny-popover';
+
+const CustomTooltip = props => {
+  const { children, text, show } = props;
+
+  return (
+    <Popover
+      isOpen={show}
+      containerClassName='custom-tooltip'
+      {...props}
+      content={({ position, targetRect, popoverRect }) => (
+        <ArrowContainer
+          position={position}
+          targetRect={targetRect}
+          popoverRect={popoverRect}
+          arrowColor='#000000CC'>
+          <div className='tooltip-content'>{text}</div>
+        </ArrowContainer>
+      )}>
+      {children}
+    </Popover>
+  );
+};
+
+export default CustomTooltip;
