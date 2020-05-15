c0311bd47116cb1592ee768ddeaf026dbcab68dc
Jakub Aniszewski <qba.anisz@gmail.com>
2020-05-15 15:32:35 +0200

    CSS refactor #74

--- src/components/pages/Notes/NoteDialog/NoteImage.js
+++ src/components/pages/Notes/NoteDialog/NoteImage.js
@@ -4,13 +4,11 @@ import { Image } from 'react-bootstrap';
 const NoteImage = ({ imageUrl, dialog }) => {
   return (
     <div className='mb-3'>
-      {dialog ? (
-        <a href={imageUrl} target='_blank' rel='noopener noreferrer'>
-          <Image src={imageUrl} id='dialog' className='note-image' rounded />
-        </a>
-      ) : (
-        <Image src={imageUrl} className='note-image' rounded />
-      )}
+      <Image
+        src={imageUrl}
+        className={dialog ? 'note-image note-image-dialog' : 'note-image'}
+        rounded
+      />
     </div>
   );
 };
