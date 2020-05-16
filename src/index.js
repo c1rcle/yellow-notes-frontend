d7c489c5cde7d5db7ca552dbea8d1553b1df866e
Wojtek <vopox07@gmail.com>
2020-05-16 12:13:29 +0200

    Install dotnev

--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import 'bootswatch/dist/litera/bootstrap.min.css';
 import './styles/custom.css';
+require('dotenv').config();
 
 ReactDOM.render(<App />, document.getElementById('root'));
