56d11e4750584df4ddd38de45d9abbf57ebbb8cb
Wojtek <vopox07@gmail.com>
2020-05-16 21:59:26 +0200

    Remove unnecessary require call

--- src/index.js
+++ src/index.js
@@ -3,6 +3,5 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import 'bootswatch/dist/litera/bootstrap.min.css';
 import './styles/custom.css';
-require('dotenv').config();
 
 ReactDOM.render(<App />, document.getElementById('root'));
