e887d8d8eb5878d7fea8f9ecb8170c4e55349423
Filip Wanat <filip.k.wanat@gmail.com>
2020-05-20 00:19:22 +0200

    Fix most of not hiding not modal on esc bug

--- src/components/pages/Notes/NoteDialog/index.js
+++ src/components/pages/Notes/NoteDialog/index.js
@@ -30,8 +30,8 @@ const NoteDialog = () => {
   };
   useTimeout(onNoteModified, formData, 1000);
 
-  const onHide = () => {
-    isNoteNew ? closeDialog() : onSubmit();
+  const onHide = e => {
+    isNoteNew ? closeDialog(e) : onSubmit(e);
   };
 
   const onSubmit = e => {
@@ -52,15 +52,23 @@ const NoteDialog = () => {
   };
 
   const onKeyDown = e => {
-    if (e.ctrlKey && e.keyCode === 13) onSubmit(e);
-    else if (e.keyCode === 13 && e.target.name === 'title') {
+    const enterKeyCode = 13;
+    const escKeyCode = 27;
+
+    if (e.ctrlKey && e.keyCode === enterKeyCode) onSubmit(e);
+    else if (e.keyCode === escKeyCode) onHide(e);
+    else if (e.keyCode === enterKeyCode && e.target.name === 'title') {
       e.preventDefault();
       e.target.form[1].focus();
     }
   };
 
   return (
-    <Modal show={dialogVisible} onHide={onHide} onKeyDown={e => onKeyDown(e)} enforceFocus={false}>
+    <Modal
+      show={dialogVisible}
+      onHide={e => onHide(e)}
+      onKeyDown={e => onKeyDown(e)}
+      enforceFocus={false}>
       <NoteDialogForm
         isNoteNew={isNoteNew}
         dialogVisible={dialogVisible}
