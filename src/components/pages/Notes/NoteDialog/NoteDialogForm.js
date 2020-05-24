8f33b9c71c6abb9fef3da577bcb8c87a9c04d7e8
Jan Miszczyk <jachuuu_15@hotmail.com>
2020-05-24 17:31:03 +0200

    Textarea is now resized automatically

--- src/components/pages/Notes/NoteDialog/NoteDialogForm.js
+++ src/components/pages/Notes/NoteDialog/NoteDialogForm.js
@@ -1,9 +1,10 @@
 import React, { useEffect, useRef } from 'react';
 import { Modal, Form } from 'react-bootstrap';
-import HoverableControl from '../../../common/controls/HoverableControl';
-import Todo from '../Todo';
-import { getVariant, getFormColor } from '../../../../utility/colorUtility';
+import TextareaAutosize from 'react-textarea-autosize';
 import NoteImage from './NoteImage';
+import Todo from '../Todo';
+import HoverableControl from '../../../common/controls/HoverableControl';
+import { getVariant, getFormColor } from '../../../../utility/colorUtility';
 import useActiveElement from '../../../../hooks/useActiveElement';
 
 const NoteDialogForm = props => {
@@ -69,16 +70,18 @@ const NoteDialogForm = props => {
       <Modal.Body style={{ backgroundColor: color }}>
         {formData.imageUrl && <NoteImage imageUrl={formData.imageUrl} dialog={true} />}
         {formData.variant === 0 ? (
-          <Form.Control
-            as='textarea'
+          <TextareaAutosize
             readOnly={isBlocked && !isNoteNew}
             name='content'
             value={content}
             placeholder='Note Content'
             onChange={e => onChange(e)}
-            rows='3'
             tabIndex='2'
-            className={`border-0 text-${getVariant(color)} placeholder-${getVariant(color)}`}
+            minRows={2}
+            maxRows={7}
+            className={`autogrow-textarea border-0 text-${getVariant(
+              color
+            )} placeholder-${getVariant(color)}`}
             style={{ backgroundColor: getFormColor(color) }}
           />
         ) : (
