3b7436dba706f760cde3f8a7a81d3ee5f396d325
Wojtek <vopox07@gmail.com>
2020-05-29 15:56:32 +0200

    Add option in category dropdown to unassign category

--- src/components/pages/Notes/NoteDialog/NoteCategoryButton.js
+++ src/components/pages/Notes/NoteDialog/NoteCategoryButton.js
@@ -5,12 +5,16 @@ import CategoryDropdown from '../CategoryBar/Dialogs/CategoryDropdown';
 
 const NoteCategoryButton = ({ setCategoryId, disabled }) => {
   const [dialogVisible, setDialogVisible] = useState(false);
+
+  const noneOption = { categoryId: -1, name: 'None' };
   const [{ categories }] = useCategories();
 
   const assignCategory = e => {
     const id = parseInt(e.target.id);
     if (Number.isInteger(id)) {
-      setCategoryId(id);
+      if (id === -1) setCategoryId(null);
+      else setCategoryId(id);
+
       setDialogVisible(false);
     }
   };
@@ -22,7 +26,7 @@ const NoteCategoryButton = ({ setCategoryId, disabled }) => {
       variant='primary'
       title='Assign to a category'
       onOptionClick={assignCategory}
-      options={categories}
+      options={[noneOption, ...categories]}
       placement='bottom'>
       <Button variant='outline-primary' onClick={() => setDialogVisible(true)} disabled={disabled}>
         <i className='fas fa-tag' />
