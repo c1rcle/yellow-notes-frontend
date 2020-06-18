f934e63d0a7f3822ee4d6941ba4bc10395de6a76
Jan Miszczyk <jachuuu_15@hotmail.com>
2020-06-18 13:43:03 +0200

    New file is now tracked

---
+++ src/components/common/OverflowTooltip.js
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from 'react';
+import CustomTooltip from './CustomTooltip';
+
+const OverflowTooltip = props => {
+  const [overflow, setOverflow] = useState(false);
+  const [hover, setHover] = useState(false);
+
+  const { children, text } = props;
+
+  let childRef = Array.isArray(children) ? children[0].ref : children.ref;
+  if (childRef === null) {
+    throw new Error('First child has to have a ref defined!');
+  }
+
+  useEffect(() => {
+    if (childRef.current.offsetWidth < childRef.current.scrollWidth) {
+      setOverflow(true);
+    } else setOverflow(false);
+  }, [childRef, hover]);
+
+  return (
+    <CustomTooltip show={hover && overflow} text={text} {...props}>
+      {React.cloneElement(children, {
+        onMouseOver: () => setHover(true),
+        onMouseOut: () => setHover(false)
+      })}
+    </CustomTooltip>
+  );
+};
+
+export default OverflowTooltip;
