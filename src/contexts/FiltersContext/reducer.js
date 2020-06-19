0155db57501dc28aa3034368d56208a4365b96c9
Jan Miszczyk <jachuuu_15@hotmail.com>
2020-06-20 01:17:59 +0200

    Removed console log

--- src/contexts/FiltersContext/reducer.js
+++ src/contexts/FiltersContext/reducer.js
@@ -12,7 +12,6 @@ export default (state, { type, payload }) => {
     }
 
     case 'REMOVE_FILTER': {
-      console.log('xd');
       const filters = state.filters.filter(f => f.categoryId !== payload.categoryId);
       return { ...state, filters: filters, needUpdate: true };
     }
